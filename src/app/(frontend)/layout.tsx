e790810436afac92d27f9eaece982c9b2815d352
Gisete Kindahl <gisete@gmail.com>
2025-09-17 08:00:59 +0100

    analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -68,6 +68,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
